bc07fd1532d366e4027ba5b95321102e251c0411
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-11-05 13:28:09 +0500

    complete Desktop Navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import {motion} from "framer-motion"
-
+import { motion } from "framer-motion";
+import { a } from "framer-motion/client";
 
 const Header = () => {
   return (
@@ -7,17 +7,17 @@ const Header = () => {
       <div className="container mx-auto px-4 py-6 flex justify-between items-center sm:px-6 lg:px-8 h-16 md:h-20">
         {/* logo */}
         <motion.div
-        initial={{opacity:0, x: -100}}
-        animate={{opacity:1, x:0}}
-        transition={{
-          type : "spring",
-          stiffness: 100,
-          damping: 25,
-          delay: 0.3,
-          duration: 1.2,
-        }}
-        
-        className=" flex items-center">
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 25,
+            delay: 0.3,
+            duration: 1.2,
+          }}
+          className=" flex items-center"
+        >
           <div className="h-10 w-10 rounded-xl bg-linear-to-r from -gray-500 to-gray-100 flex items-center justify-center text-purple-600 font-bold text-xl mr-3 ">
             H
           </div>
@@ -26,6 +26,31 @@ const Header = () => {
             Haseeb
           </span>
         </motion.div>
+
+        {/* Desktop Navigation */}
+
+        <nav className="lg:flex hidden space-x-8">
+          {["Home", "About", "Projects", "Contact"].map((item,index)=>
+          <motion.a 
+          key={item}
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
+            delay: 0.7 + index * 0.2,
+            duration: 1.2,
+          }}
+          className="relative text-gray-800 dark:text-gray-200 hover:text-violet-600 dark:hover:text-violet-400 font-medium transition-colors duration-300 group"
+          href="#">
+            {item}
+            <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-violet-600 group-hover:w-full transition-all duration-300"></span>
+
+          </motion.a>
+          
+          )}
+        </nav>
       </div>
     </header>
   );
