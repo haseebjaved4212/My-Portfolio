f45bf79c9586811d926d4b3b0c356e6d5c0e03f0
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-11-12 13:30:46 +0500

    Coplete A section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -91,6 +91,22 @@ const Header = () => {
             <FiLinkedin className="w-5 h-5" />
           </motion.a>
         </div>
+
+        {/* Hire Me Button*/}
+        <motion.button
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          delay: 1.6,
+          duration: 0.8,
+          type: "spring",
+          stiffness: 100,
+          damping: 15,
+        }}
+        
+        className="ml-4 px-4 py-2 rounded-xl bg-linear-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-300 
+        "
+        >Hire Me</motion.button>
       </div>
     </header>
   );
