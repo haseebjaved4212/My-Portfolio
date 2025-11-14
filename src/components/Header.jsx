677943a834a22278d2edfa6463671ca957068ea1
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-11-14 15:44:16 +0500

    Coplete A section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -97,29 +97,35 @@ const Header = () => {
           >
             <FiLinkedin className="w-5 h-5" />
           </motion.a>
+
+
+          
+          {/* Hire Me Button*/}
+          <motion.button
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              delay: 1.6,
+              duration: 0.8,
+              type: "spring",
+              stiffness: 100,
+              damping: 15,
+            }}
+            className="ml-4 px-4 py-2 rounded-xl bg-linear-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-300 
+        "
+          >
+            Hire Me
+          </motion.button>
         </div>
 
-        {/* Hire Me Button*/}
-        <motion.button
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            delay: 1.6,
-            duration: 0.8,
-            type: "spring",
-            stiffness: 100,
-            damping: 15,
-          }}
-          className="ml-4 px-4 py-2 rounded-xl bg-linear-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-300 
-        "
-        >
-          Hire Me
-        </motion.button>
+        {/* Mobile Navigation */}
         <div className="md:hidden flex items-center">
-          <button>{isOpen ? <FiX /> : <FiMenu />}</button>
+          <motion.button className="text-gray-300 cursor-pointer"
+          onClick={toggleMenu}
+          
+          >{isOpen ? <FiX classname="h-5 w-5" /> : <FiMenu  classname="h-6 w-6"/>}</motion.button>
         </div>
       </div>
-      {/* Mobile Navigation */}
     </header>
   );
 };
