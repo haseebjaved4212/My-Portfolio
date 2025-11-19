9e97750a42b63c999c209fafa5e904aabada9777
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-11-19 11:44:07 +0500

    Complete A section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
- import Header from './components/Header.jsx'
- 
+import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
+
+
 const App = () => {
   return (
     <>  
       <Header />
-      
+      <Hero />
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+
+
+const Hero = () => {
+  return (
+    <section className="h-screen bg-linear-to-b from-violet-900 to black"></section>
+  )
+}
+
+export default Hero
