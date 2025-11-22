83556ddcd007db547caff26d727a010f4b072336
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-11-22 12:57:35 +0500

    Complete A section

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -1,39 +1,50 @@
-import { useRef, useEffect } from "react"
-import { gsap } from "gsap"
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
 const CustomCursor = () => {
-    const cursorRef = useRef(null)
-    const cursorBorderRef = useRef(null)
+  const cursorRef = useRef(null);
+  const cursorBorderRef = useRef(null);
+
+  // Hide Cursor On Mobile
+  const isMobile =
+    typeof window !== "undefined" &&
+    window.matchMedia("(max-width: 768px)").matches;
+
+  if (isMobile) return null;
+
+  useEffect(() => {
+    // Get Cursor Element
+    const cursor = cursorRef.current;
+    const cursorBorder = cursorBorderRef.current;
+
+    // Initial Position Off-screen
+    gsap.set([cursor, cursorBorder], {
+    
+      xPercent: 50,
+      yPercent: 50,
+    });
+
+    //  Variables  For Cursor position With different  Speed 
 
 
-    // Hide Cursor On Mobile 
-    const isMobile = typeof window !== "undefined" && window.matchMedia ("(max-width: 768px)").matches
 
 
-    if (isMobile) return null
 
-    useEffect(() => {
-        // Get Cursor Element 
-        const cursor = cursorRef.current
-        const cursorBorder = cursorBorderRef.current
-        
-    })
-    return (
-        <>
-            {/* Main Cursor Dot  */}
-            <div
-                ref={cursorRef}
-                className="fixed top-0 left-0 w-[20px] h-[20px]  rounded-full bg-white pointer-events-none z-999 mix-blend-difference"
+  });
+  return (
+    <>
+      {/* Main Cursor Dot  */}
+      <div
+        ref={cursorRef}
+        className="fixed top-0 left-0 w-5  h-5 rounded-full bg-white pointer-events-none z-999 mix-blend-difference"
+      />
+      {/* Cursor Border */}
+      <div
+        ref={cursorBorderRef}
+        className="fixed top-0 left-0 w-10 h-10   rounded-full border  border-white pointer-events-none z-999 mix-blend-difference opacity-50"
+      />
+    </>
+  );
+};
 
-            />
-            {/* Cursor Border */}
-            <div
-                ref={cursorBorderRef}
-                className="fixed top-0 left-0 w-[40px] h-[40px]  rounded-full border  border-white pointer-events-none z-999 mix-blend-difference opacity-50"
-
-            />
-        </>
-    )
-}
-
-export default CustomCursor
+export default CustomCursor;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,8 +15,6 @@ const Header = () => {
   const openContactForm = () => setContactFormOpen(true);
   const closeContactForm = () => setContactFormOpen(false);
 
-
-
   // Contact ForM
   // const [name, setName] = useState("");
   // const [email, setEmail] = useState("");
@@ -111,8 +109,6 @@ const Header = () => {
             <FiLinkedin className="w-5 h-5" />
           </motion.a>
 
-
-
           {/* Hire Me Button*/}
           <motion.button
             onClick={openContactForm}
@@ -136,12 +132,15 @@ const Header = () => {
         <div className="md:hidden flex items-center">
           <motion.button
             whileTap={{ scale: 0.8 }}
-
-
             className="text-gray-300 cursor-pointer"
             onClick={toggleMenu}
-
-          >{isOpen ? <FiX classname="h-8 w-8" /> : <FiMenu className="h-8 w-8" />}</motion.button>
+          >
+            {isOpen ? (
+              <FiX classname="h-8 w-8" />
+            ) : (
+              <FiMenu className="h-8 w-8" />
+            )}
+          </motion.button>
         </div>
       </div>
       {/* Mobile Menu */}
@@ -149,21 +148,15 @@ const Header = () => {
         initial={{ opacity: 0, height: 0 }}
         animate={{ opacity: isOpen ? 1 : 0, height: isOpen ? "auto" : 0 }}
         transition={{
-
           duration: 0.5,
         }}
-        className="md:hidden overflow-hidden bg-white dark:bg-gray-900 shadow-lg px-4 py-5 space-y-5">
+        className="md:hidden overflow-hidden bg-white dark:bg-gray-900 shadow-lg px-4 py-5 space-y-5"
+      >
         <nav className="flex flex-col space-y-3">
-          {[
-            "Home",
-            "About",
-            "Projects",
-            "Contact",
-          ].map((item, index) => (
+          {["Home", "About", "Projects", "Contact"].map((item) => (
             <motion.a
               onClick={toggleMenu}
               key={item}
-
               className="relative text-gray-800 dark:text-gray-200 hover:text-violet-600 dark:hover:text-violet-400 font-medium transition-colors duration-300 group"
               href="#"
             >
@@ -186,23 +179,19 @@ const Header = () => {
             </a>
           </div>
           <button
-            onClick={() => { toggleMenu()
-              openContactForm()
-
-
-             }}
-
-            className="w-full mt-5 px-4 py-2 rounded-xl bg-linear-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-300 ">
+            onClick={() => {
+              toggleMenu();
+              openContactForm();
+            }}
+            className="w-full mt-5 px-4 py-2 rounded-xl bg-linear-to-r from-gray-400 to-gray-100 text-violet-700 font-bold hover:from-violet-700 hover:to-purple-700 hover:text-white transition-all duration-300 "
+          >
             Hire Me
           </button>
         </div>
       </motion.div>
 
-
       {/* Contact Form  */}
-      <AnimatePresence >
-
-
+      <AnimatePresence>
         {ContactFormOpen && (
           <motion.div
             initial={{ opacity: 0 }}
@@ -211,7 +200,7 @@ const Header = () => {
             transition={{ duration: 0.5 }}
             className="fixed inset-0 bg-black/50 
         background-blur-sm  flex items-center justify-center z-50 p-4 "
-          // onClick={closeContactForm}
+            // onClick={closeContactForm}
           >
             <motion.div
               initial={{ scale: 0.8, opacity: 0, y: 30 }}
@@ -221,40 +210,78 @@ const Header = () => {
                 type: "spring",
                 damping: 30,
                 stiffness: 200,
-                duration: 0.8
-
+                duration: 0.8,
               }}
-              className="bg-white max-w-md w-full dark:bg-gray-900 p-6 rounded-lg  shadow-xl">
-
-
+              className="bg-white max-w-md w-full dark:bg-gray-900 p-6 rounded-lg  shadow-xl"
+            >
               <div className="flex justify-between items-center mb-4">
                 <h2 className="text-2xl font-bold">Get in Touch</h2>
-                <button onClick={closeContactForm} className="text-gray-700 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-300"><FiX className="w-5 h-5" /></button>
+                <button
+                  onClick={closeContactForm}
+                  className="text-gray-700 dark:text-gray-300 hover:text-violet-600 dark:hover:text-violet-400 transition-colors duration-300"
+                >
+                  <FiX className="w-5 h-5" />
+                </button>
               </div>
               <form action="">
                 <div className="mb-4">
-                  <label htmlFor="name" className="block text-gray-700 dark:text-gray-300 font-medium mb-2">Name</label>
-                  <input type="text" id="name" name="name" className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-600 focus:border-violet-600" placeholder="Enter your name" />
+                  <label
+                    htmlFor="name"
+                    className="block text-gray-700 dark:text-gray-300 font-medium mb-2"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-600 focus:border-violet-600"
+                    placeholder="Enter your name"
+                  />
                 </div>
                 <div className="mb-4">
-                  <label htmlFor="email" className="block text-gray-700 dark:text-gray-300 font-medium mb-2">Email</label>
-                  <input type="email" id="email" name="email" className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-600 focus:border-violet-600" placeholder="Enter your email" />
+                  <label
+                    htmlFor="email"
+                    className="block text-gray-700 dark:text-gray-300 font-medium mb-2"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-600 focus:border-violet-600"
+                    placeholder="Enter your email"
+                  />
                 </div>
                 <div className="mb-4">
-                  <label htmlFor="message" className="block text-gray-700 dark:text-gray-300 font-medium mb-2">Message</label>
-                  <textarea id="message" name="message" rows="4" className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-600 focus:border-violet-600" placeholder="Enter your message"></textarea>
+                  <label
+                    htmlFor="message"
+                    className="block text-gray-700 dark:text-gray-300 font-medium mb-2"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows="4"
+                    className="w-full px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-lg focus:outline-none focus:ring-2 focus:ring-violet-600 focus:border-violet-600"
+                    placeholder="Enter your message"
+                  ></textarea>
                 </div>
                 <motion.button
                   whileHover={{ scale: 1.03 }}
                   whileTap={{ scale: 0.98 }}
-                  type="submit" className="w-full px-4 py-2 bg-violet-600 text-white font-medium rounded-lg hover:bg-gray-100  hover:text-violet-700 transition-colors duration-300">Send Message</motion.button>
+                  type="submit"
+                  className="w-full px-4 py-2 bg-violet-600 text-white font-medium rounded-lg hover:bg-gray-100  hover:text-violet-700 transition-colors duration-300"
+                >
+                  Send Message
+                </motion.button>
               </form>
             </motion.div>
           </motion.div>
         )}
       </AnimatePresence>
-
-
     </header>
   );
 };
