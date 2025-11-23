ed857a4174b54351583488b0fc6f980afeafd938
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-11-23 11:55:36 +0500

    Complete A section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@
 import Header from './components/Header.jsx'
 import Hero from './components/Hero.jsx'
 import CustomCursor from './components/CustomCursor.jsx'
-
+import About from './components/About.jsx'
 const App = () => {
   return (
     <>  
       <Header />
       <Hero />
       <CustomCursor />
+      <About />
     </>
   )
 }
