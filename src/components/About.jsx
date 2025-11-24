0f2abe90b27268a9cfda44377d0c7cec08f72a4d
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-11-24 08:42:52 +0500

    Complete A section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -54,8 +54,27 @@ const About = () => {
   return (
     <section
       ref={sectionRef}
-      className="h-screen relative bg-gradient-to-b from-black to-[#9a74cf50]  "
+      className="h-screen relative bg-linear-to-b from-black to-[#9a74cf50]  "
     >
+      {/* Stars */}
+      <div className="absolute inset-0 overflow-hidden ">
+        {[...Array(10)].map((_, i) => (
+          <div key={`star-${i}`} 
+          className="absolute rounded-full "
+          style={{
+            width: `${10 + i * 3}px`,
+            height: `${10 + i * 3}px`,
+            left: `${Math.random() * 100}%`,
+            top: `${Math.random() * 100}%`,
+            opacity : 0.2 + Math.random() * 0.4,
+            backgroundColor: "white" 
+
+
+          }}
+          />
+        ))}
+      </div>
+
       <div className="container mx-auto px-4 h-full flex flex-col items-center justify-start pt-16">
         <h1
           ref={titleRef}
@@ -65,27 +84,27 @@ const About = () => {
         </h1>
 
         <div
-  ref={introRef}
-  className="mt-8 mx-4 w-full flex flex-col items-center gap-10 md:gap-16 md:flex-row md:items-center md:justify-between opacity-0"
->
-  <h3 className="text-sm md:text-2xl font-bold text-purple-200 max-w-[40rem] text-center md:text-left tracking-wider">
-    I'm a front-end developer passionate about creating modern,
-    high-performance digital experiences. My journey began with HTML,
-    CSS, and JavaScript, and has grown into a strong skill set across
-    TypeScript, React.js, Next.js, Three.js, GSAP, Spline, and
-    automation with n8n. I enjoy blending clean engineering with
-    creative visuals to build interfaces that feel smooth, responsive,
-    and meaningful. I also have a solid foundation in computer
-    networking and am currently expanding my expertise into backend
-    development to become a more complete, end-to-end developer.
-  </h3>
+          ref={introRef}
+          className="mt-8 mx-4 w-full flex flex-col items-center gap-10 md:gap-16 md:flex-row md:items-center md:justify-between opacity-0"
+        >
+          <h3 className="text-sm md:text-2xl font-bold text-purple-200 max-w-[40rem] text-center md:text-left tracking-wider">
+            I'm a front-end developer passionate about creating modern,
+            high-performance digital experiences. My journey began with HTML,
+            CSS, and JavaScript, and has grown into a strong skill set across
+            TypeScript, React.js, Next.js, Three.js, GSAP, Spline, and
+            automation with n8n. I enjoy blending clean engineering with
+            creative visuals to build interfaces that feel smooth, responsive,
+            and meaningful. I also have a solid foundation in computer
+            networking and am currently expanding my expertise into backend
+            development to become a more complete, end-to-end developer.
+          </h3>
 
-  <img
-    className="h-80 md:h-96 lg:h-[26rem] mix-blend-lighten"
-    src="Images/person.png"
-    alt="Profile Picture"
-  />
-</div>
+          <img
+            className="h-80 md:h-96 lg:h-[26rem] mix-blend-lighten"
+            src="Images/person.png"
+            alt="Profile Picture"
+          />
+        </div>
       </div>
     </section>
   );
