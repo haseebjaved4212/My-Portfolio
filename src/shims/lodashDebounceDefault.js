ee3ab9b33a387ef10d788e45e15bea9a5f31b6ff
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-11-25 13:28:10 +0500

    Complete A Section

---
+++ src/shims/lodashDebounceDefault.js
@@ -0,0 +1,5 @@
+// Shim to always provide a default export for lodash.debounce
+import debounce from 'lodash.debounce';
+
+export default debounce;
+export { debounce };
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      'lodash.debounce': '/src/shims/lodashDebounceDefault.js',
+    },
+  },
+  optimizeDeps: {
+    exclude: [
+      'gsap',
+      '@gsap/react',
+      '@splinetool/react-spline',
+      '@splinetool/runtime',
+      'framer-motion',
+    ],
+  },
 })
